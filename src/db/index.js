0413a0e1b13581755532a6460839d23c8d450765
RushikeshTonape <rushikeshtonape143@gmail.com>
2024-01-02 11:35:52 +0530

    created User Model and Video Model

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
+import colors from "colors";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+        .bgMagenta
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ".bgMagenta, error);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timrstamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);

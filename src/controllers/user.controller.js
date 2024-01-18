5d1050c844bb1129c7e0918da15c61bacd609dda
RushikeshTonape <rushikeshtonape143@gmail.com>
2024-01-18 23:02:16 +0530

    add functionality to change password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -207,4 +207,22 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?.id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Please enter correct passord");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Password Changed Succefully"));
+});
+
 export { RegisterUser, loginUser, logoutUser, refreshAccessToken };
